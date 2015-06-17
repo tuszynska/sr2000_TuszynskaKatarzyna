ff4321cc3f5444ca99a24b883bf152e1ff15dd54
maciejanowski <maciejanowski@gmail.com>
2015-06-17 18:44:43 +0200

    Zorro tu był

---
+++ zorro.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author student204
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("********************\n"
+                + "                  *\n"
+                + "                 *\n"
+                + "                *\n"
+                + "               *\n"
+                + "              *\n"
+                + "             *\n"
+                + "            *\n"
+                + "           *\n"
+                + "          *\n"
+                + "         *\n"
+                + "        *\n"
+                + "       *\n"
+                + "      *\n"
+                + "     *\n"
+                + "    *\n"
+                + "   *\n"
+                + "  *\n"
+                + " ***********************");
+  }// TODO code application logic here
+    }
+    
+}
